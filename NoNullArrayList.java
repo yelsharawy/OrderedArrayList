8d98b508ff05b98c1b5f2d7a14dca77c91689025
yelsharawy <yelsharawy20@stuy.edu>
2020-12-09 13:02:40 -0500

    Implemented NoNullArrayList add methods

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -6,4 +6,20 @@ public class NoNullArrayList<E extends Comparable<E>> extends ArrayList<E> {
         super();
     }
 
+    public boolean add(E e) {
+        checkValid(e);
+        return super.add(e);
+    }
+
+    public void add(int index, E element) {
+        checkValid(element);
+        super.add(index, element);
+    }
+
+
+    private void checkValid(E e) {
+        if (e == null) {
+            throw new IllegalArgumentException("cannot add null to NoNullArrayList");
+        }
+    }
 }
