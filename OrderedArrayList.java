fb33b9b2b41c0bdec05950db4acb8276dcc73f74
yelsharawy <yelsharawy20@stuy.edu>
2020-12-09 13:07:58 -0500

    Added OrderedArrayList.java with constructors

---
+++ OrderedArrayList.java
@@ -0,0 +1,11 @@
+public class OrderedArrayList<E extends Comparable<E>> extends NoNullArrayList<E> {
+
+    public OrderedArrayList() {
+        super();
+    }
+
+    public OrderedArrayList(int initialCapacity) {
+        super(initialCapacity);
+    }
+
+}
