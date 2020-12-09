4b197c1cb68943b1c14fad30a87918c12d457d6c
yelsharawy <yelsharawy20@stuy.edu>
2020-12-09 13:25:56 -0500

    Implemented OrderedArrayList add methods

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -8,4 +8,26 @@ public class OrderedArrayList<E extends Comparable<E>> extends NoNullArrayList<E
         super(initialCapacity);
     }
 
+    public boolean add(E e) {
+        super.add(findInsertIndex(e), e);
+        return true;
+    }
+
+    public void add(int index, E element) {
+        add(element);
+    }
+
+
+    private int findInsertIndex(E element) {
+        //* simple linear search
+        int size = size();
+        for (int i = 0; i < size; i++) {
+            if (get(i).compareTo(element) > 0) {
+                return i;
+            }
+        }
+        return size;
+        //*/
+    }
+
 }
