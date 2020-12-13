3b7d6580b83c98c1c33c916ec31f766b6532265d
yelsharawy <yelsharawy20@stuy.edu>
2020-12-13 01:04:05 -0500

    Fixed generic type of NoNullArrayList

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class NoNullArrayList<E extends Comparable<E>> extends ArrayList<E> {
+public class NoNullArrayList<E> extends ArrayList<E> {
 
     public NoNullArrayList() {
         super();
